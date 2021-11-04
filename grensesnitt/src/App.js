b868696bcd22cc138f9538c8e7ef823497455535
prince7866 <71831042+prince7866@users.noreply.github.com>
2021-11-04 22:18:16 +0100

    Update som rules

--- grensesnitt/src/App.js
+++ grensesnitt/src/App.js
@@ -1,5 +1,12 @@
 import HomePage from "./pages/HomePage/HomePage";
 
+//Main component
+
+
+//Code uses BEM convention to name CSS classes
+//className uses camelCase
+//Each component has its own css file
+
 
 function App() {
   return (
