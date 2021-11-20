4bb70f5443102acd8460450d47faf42abde49205
KidRonee <mathias-b@hotmail.no>
2021-11-20 18:51:58 +0100

    preparing for pull request

--- grensesnitt/src/pages/AboutUsPage/AboutUsPage.jsx
+++
@@ -1,9 +0,0 @@
-import "./AboutUsPage.css";
-
-import React from "react";
-
-function AboutUsPage() {
-  return <div className="aboutUsPage"></div>;
-}
-
-export default AboutUsPage;
--- grensesnitt/src/pages/MenuPage/MenuPage.jsx
+++
@@ -1,9 +0,0 @@
-import "/MenuPage.css";
-
-import React from "react";
-
-function MenuPage() {
-  return <div className="menuPage"></div>;
-}
-
-export default MenuPage;
