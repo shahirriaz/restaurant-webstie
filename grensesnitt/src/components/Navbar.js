ad7a7b5987fc97bee05f5c7f871c55213b7e2931
KidRonee <mathias-b@hotmail.no>
2021-11-20 17:20:33 +0100

    added functionality to menu link in navbar

--- grensesnitt/src/components/Navbar.js
+++ grensesnitt/src/components/Navbar.js
@@ -1,7 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { Button } from './Button';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React, { useState, useEffect } from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { scroller } from "react-scroll";
+
+const scrollToMenu = () => {
+  scroller.scrollTo("menu-container", {
+    duration: 800,
+    delay: 0,
+    smooth: "easeInOutQuart",
+  });
+};
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -22,60 +31,52 @@ function Navbar() {
     showButton();
   }, []);
 
-  window.addEventListener('resize', showButton);
+  window.addEventListener("resize", showButton);
 
   return (
     <>
-      <nav className='navbar'>
-        <div className='navbar-container'>
-        <ul className={click ? 'nav-menu-1 active' : 'nav-menu-1'}>
-            <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <ul className={click ? "nav-menu-1 active" : "nav-menu-1"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
                 OM OSS
               </Link>
             </li>
-            <li className='nav-item'>
-              <Link
-                to='/'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={scrollToMenu}>
                 MENY
               </Link>
             </li>
-            <li className='nav-item'>
-              <Link
-                to='/cart'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
+            <li className="nav-item">
+              <Link to="/cart" className="nav-links" onClick={closeMobileMenu}>
                 <i class="fas fa-shopping-cart"></i>
               </Link>
             </li>
           </ul>
-          <Link to='/' onClick={closeMobileMenu}>
-            <img src="images/pizzeria_bella.png" id="logo" alt="Pizzeria Bella logo" />
+          <Link to="/" onClick={closeMobileMenu}>
+            <img
+              src="images/pizzeria_bella.png"
+              id="logo"
+              alt="Pizzeria Bella logo"
+            />
           </Link>
-          <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
-          <ul className={click ? 'nav-menu-2 active' : 'nav-menu-2'}>
-            <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-              <i class="fab fa-instagram"></i>
+          <ul className={click ? "nav-menu-2 active" : "nav-menu-2"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                <i class="fab fa-instagram"></i>
               </Link>
             </li>
-            <li className='nav-item'>
-              <Link
-                to='/'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
                 <i class="fab fa-facebook"></i>
               </Link>
             </li>
           </ul>
-          {button && <Button buttonStyle='btn--outline'>BESTILL</Button>}
+          {button && <Button buttonStyle="btn--outline">BESTILL</Button>}
         </div>
       </nav>
     </>
