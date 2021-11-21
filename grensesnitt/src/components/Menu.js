f2c2cf6e10ecc91672ee27907bcb74a6f068db77
prince7866 <71831042+prince7866@users.noreply.github.com>
2021-11-21 19:26:07 +0100

    Refactor menu

--- grensesnitt/src/components/Menu.js
+++ grensesnitt/src/components/Menu.js
@@ -3,235 +3,216 @@ import "../App.css";
 import { Button } from "./Button";
 import "./Menu.css";
 
+const pizzaItems = [
+  {
+    item: (
+      <div className="pizza-item">
+        <h3>MARGHERITA</h3>
+        <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
+        <p>119,-/149</p>
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Legg til
+        </Button>
+      </div>
+    ),
+  },
+  {
+    item: (
+      <div className="pizza-item">
+        <h3>BIFF</h3>
+        <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
+        <p>119,-/149</p>
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Legg til
+        </Button>
+      </div>
+    ),
+  },
+  {
+    item: (
+      <div className="pizza-item">
+        <h3>BIFF</h3>
+        <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
+        <p>119,-/149</p>
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Legg til
+        </Button>
+      </div>
+    ),
+  },
+  {
+    item: (
+      <div className="pizza-item">
+        <h3>BIFF</h3>
+        <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
+        <p>119,-/149</p>
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Legg til
+        </Button>
+      </div>
+    ),
+  },
+  {
+    item: (
+      <div className="pizza-item">
+        <h3>BIFF</h3>
+        <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
+        <p>119,-/149</p>
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Legg til
+        </Button>
+      </div>
+    ),
+  },
+  {
+    item: (
+      <div className="pizza-item">
+        <h3>BIFF</h3>
+        <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
+        <p>119,-/149</p>
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Legg til
+        </Button>
+      </div>
+    ),
+  },
+  {
+    item: (
+      <div className="pizza-item">
+        <h3>BIFF</h3>
+        <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
+        <p>119,-/149</p>
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Legg til
+        </Button>
+      </div>
+    ),
+  },
+  {
+    item: (
+      <div className="pizza-item">
+        <h3>BIFF</h3>
+        <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
+        <p>119,-/149</p>
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Legg til
+        </Button>
+      </div>
+    ),
+  },
+  {
+    item: (
+      <div className="pizza-item">
+        <h3>BIFF</h3>
+        <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
+        <p>119,-/149</p>
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Legg til
+        </Button>
+      </div>
+    ),
+  },
+  {
+    item: (
+      <div className="pizza-item">
+        <h3>BIFF</h3>
+        <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
+        <p>119,-/149</p>
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Legg til
+        </Button>
+      </div>
+    ),
+  },
+  {
+    item: (
+      <div className="pizza-item">
+        <h3>BIFF</h3>
+        <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
+        <p>119,-/149</p>
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Legg til
+        </Button>
+      </div>
+    ),
+  },
+  {
+    item: (
+      <div className="pizza-item">
+        <h3>BIFF</h3>
+        <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
+        <p>119,-/149</p>
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Legg til
+        </Button>
+      </div>
+    ),
+  },
+];
+
+const renderPizzaItem = () => {
+  return (
+    <>
+      {pizzaItems.map((pizzaItem) => {
+        return pizzaItem.item;
+      })}
+    </>
+  );
+};
+
 function Menu() {
   return (
     <div className="menu-container">
       <h1 className="pizza-header">PIZZAS</h1>
-      <div className="pizza-container">
-        <div className="pizza-item">
-          <h3>MARGHERITA</h3>
-          <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
-          <p>119,-/149</p>
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Legg til
-          </Button>
-        </div>
-        <div className="pizza-item">
-          <h3>MARGHERITA</h3>
-          <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
-          <p>119,-/149</p>
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Legg til
-          </Button>
-        </div>
-        <div className="pizza-item">
-          <h3>MARGHERITA</h3>
-          <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
-          <p>119,-/149</p>
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Legg til
-          </Button>
-        </div>
-        <div className="pizza-item">
-          <h3>MARGHERITA</h3>
-          <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
-          <p>119,-/149</p>
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Legg til
-          </Button>
-        </div>
-        <div className="pizza-item">
-          <h3>MARGHERITA</h3>
-          <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
-          <p>119,-/149</p>
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Legg til
-          </Button>
-        </div>
-        <div className="pizza-item">
-          <h3>MARGHERITA</h3>
-          <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
-          <p>119,-/149</p>
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Legg til
-          </Button>
-        </div>
-        <div className="pizza-item">
-          <h3>MARGHERITA</h3>
-          <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
-          <p>119,-/149</p>
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Legg til
-          </Button>
-        </div>
-        <div className="pizza-item">
-          <h3>MARGHERITA</h3>
-          <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
-          <p>119,-/149</p>
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Legg til
-          </Button>
-        </div>
-        <div className="pizza-item">
-          <h3>MARGHERITA</h3>
-          <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
-          <p>119,-/149</p>
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Legg til
-          </Button>
-        </div>
-        <div className="pizza-item">
-          <h3>MARGHERITA</h3>
-          <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
-          <p>119,-/149</p>
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Legg til
-          </Button>
-        </div>
-        <div className="pizza-item">
-          <h3>MARGHERITA</h3>
-          <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
-          <p>119,-/149</p>
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Legg til
-          </Button>
-        </div>
-        <div className="pizza-item last-pizza-item">
-          <h3>MARGHERITA</h3>
-          <p>Tomatsaus, fersk mozzarella og fersk basilikum.</p>
-          <p>119,-/149</p>
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Legg til
-          </Button>
-        </div>
-      </div>
-      <h1 className="pasta-header">PASTA</h1>
-      <div className="pasta-container">
-        <div className="pasta-item">
-          <h3>BIGOLI ALLA PUTTANESCA</h3>
-          <p>Lang pasta med Puttanesca</p>
-          <p>163,-</p>
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Legg til
-          </Button>
-        </div>
-        <div className="pasta-item">
-          <h3>LINGUINE ALLA PESCATORA</h3>
-          <p>Linguine med blåskjell, scampi og blekksprut</p>
-          <p>210,-</p>
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Legg til
-          </Button>
-        </div>
-        <div className="pasta-item">
-          <h3>RAVIOLI TRICOLORE</h3>
-          <p>
-            Pastaputer fylt med mozzarella, San Marzano DOP tomater og
-            basilikumpesto, servert med tomatsaus, burratakrem og fersk
-            basilikum
-          </p>
-          <p>229,-</p>
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Legg til
-          </Button>
-        </div>
-        <div className="pasta-item">
-          <h3>PAPPARDELLE AL RAGU DI CINGHIALE</h3>
-          <p>Pappadelle med villsvinragu</p>
-          <p>235,-</p>
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Legg til
-          </Button>
-        </div>
-        <div className="pasta-item">
-          <h3>RIGATONI PARTENOPEI</h3>
-          <p>Rigatoni med salsiccia, ricotta og friarielli</p>
-          <p>179,-</p>
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Legg til
-          </Button>
-        </div>
-        <div className="pasta-item">
-          <h3>RIGATONI CACIO PEPE</h3>
-          <p>Kort pasta med peccorino, parmesan og pepper</p>
-          <p>193,-</p>
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Legg til
-          </Button>
-        </div>
-      </div>
+      <div className="pizza-container">{renderPizzaItem()}</div>
     </div>
   );
 }
